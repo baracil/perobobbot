adf96c67f96fddbf163cc14a9068a7ae719c0de2
Bastien Aracil <findbastien@gmail.com>
2022-08-18 23:23:54 +0200

    Initialize project skeleton (add missing files)

---
+++ perobobbot.server.launcher/src/main/java/perobobbot/server/launcher/Perobobbot.java
@@ -0,0 +1,9 @@
+package perobobbot.server.launcher;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Perobobbot {
+    public static void main(String[] args) {
+        Micronaut.run(Perobobbot.class, args);
+    }
+}
